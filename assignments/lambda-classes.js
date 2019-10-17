f5561d15e39756f02b852d0074b687bbe34b6999
Ashley Dunham <xenocide21@gmail.com>
2019-10-17 15:12:21 -0600

    prototype refactor complete, lambda classes started

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,41 @@
 // CODE here for your Lambda Classes
+class Person{
+    constructor(properties){
+        this.name = properties.name;
+        this.age = properties.age;
+        this.location = properties.location;
+    }
+    speak(){
+        return `Hello my name is ${this.name}, I am from ${this.location}`
+    }
+}
+
+class Instructor extends Person{
+    constructor(properties){
+        super(properties);
+        this.specialty = properties.specialty;
+        this.favLang = properties.favLang;
+        this.catchPhrase = properties.catchPhrase;
+    }
+    demo(subject){
+        return `Today we are learning about ${subject}`
+    }
+    grade(Student, subject){
+        return `${Student.name} receives a perfect score on ${subject}`
+    }
+}
+
+class Student extends Person{
+    constructor(properties){
+        super(properties);
+        this.previousBackground = properties.previousBackground;
+        this.className = properties.className;
+        this.favSubjects = properties.favSubjects;
+    }
+    listsSubjects(){
+        return `${this.favSubjects}`
+    }
+    PRassignment(subject){
+        return `${Student.name} has submitted a PR for ${subject}`
+    }
+}
